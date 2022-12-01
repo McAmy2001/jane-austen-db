6b271cdda1ab3ba3259249617ee08aaa97b3cf1e
Amy McCabe <mcca0168@umn.edu>
2022-12-01 14:34:31 -0600

    db.sql, db.schema, db.seeds started

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS janeausten;
+CREATE DATABASE janeausten;
+USE janeausten;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO novels (title, year, complete)
+VALUES 
+  ('Sense and Sensibility', 1811, 1),
+  ('Pride and Prejudice', 1813, 1),
+  ('Mansfield Park', 1814, 1),
+  ('Emma', 1815, 1),
+  ('Northanger Abbey', 1818, 1),
+  ('Persuasion', 1818, 1),
+  ('Lady Susan', 1871, 1),
+  ('The Watsons', 1804, 0),
+  ('Sanditon', 1817, 0);

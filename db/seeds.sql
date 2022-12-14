870bad88372615eec76ed085784e2b48e51b24d9
Amy McCabe <mcca0168@umn.edu>
2022-12-14 15:41:05 -0600

    added characters table and started seeding it

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,3 +9,18 @@ VALUES
   ('Lady Susan', 1871, 1),
   ('The Watsons', 1804, 0),
   ('Sanditon', 1817, 0);
+
+INSERT INTO characters (first_name, last_name, novel_id)
+VALUES
+  ('Elizabeth', 'Bennett', 2),
+  ('Fitzwilliam', 'Darcy', 2),
+  ('Anne', 'Elliot', 6),
+  ('Frederick', 'Wentworth', 6),
+  ('Elinor', 'Dashwood', 1),
+  ('Marianne', 'Dashwood', 1),
+  ('Edward', 'Ferras', 1),
+  ('Christopher', 'Brandon', 1),
+  ('Fanny', 'Price', 3),
+  ('Edward', 'Bertram', 3),
+  ('Emma', 'Woodhouse', 4),
+  ('George', 'Knightley', 4);
